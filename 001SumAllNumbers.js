a20d1d725e6de0305c33eaab6e76cf925b9a61a2
Arjay Arceo <arjay.m.arceo@gmail.com>
2023-09-13 13:18:25 +0800

    new comment

--- 001SumAllNumbers.js
+++ 001SumAllNumbers.js
@@ -4,7 +4,10 @@ Return the sum of those two numbers plus the sum of all the numbers between them
 The lowest number will not always come first.
 
 For example, sumAll([4,1]) should return 10 because sum of all the numbers 
-between 1 and 4 (both inclusive) is 10. */
+between 1 and 4 (both inclusive) is 10. 
+
+another line
+*/
 
 function sumAll(arr) {
     return 1;
